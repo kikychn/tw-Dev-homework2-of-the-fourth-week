49f4f3fdbcf062eca8ca5478d486c4f27eb991be
kikychn <kikychn@gmail.com>
2019-04-19 21:58:35 +0800

    achieve the function of fizzBuzz() with special digit 3

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -2,6 +2,9 @@ package com.thoughtworks.tdd;
 
 public class FizzBuzz {
     public String fizzBuzz(int i) {
+        if (i % 3 == 0) {
+            return "Fizz";
+        }
         return String.valueOf(i);
     }
 }
